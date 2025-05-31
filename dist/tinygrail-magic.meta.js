51506b02e2644ff706de2f49e214413a5304e46d
yukitsubute <1318844659@qq.com>
2025-05-31 16:42:31 +0800

    chore: 更新.gitignore

---
+++ dist/tinygrail-magic.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Tinygrail Magic
+// @namespace    https://github.com/koorinyaa/tinygrail-magic
+// @version      1.0.0
+// @author       koorinyaa
+// @description  全新的小圣杯界面
+// @icon         https://tinygrail.com/favicon.ico
+// @match        *://*.bgm.tv/*
+// @match        *://*.bangumi.tv/*
+// @match        *://*.chii.in/*
+// @grant        GM_addStyle
+// ==/UserScript==
