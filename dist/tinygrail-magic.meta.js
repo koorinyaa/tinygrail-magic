12496b0619f848d63df05187c18a35b2b22ad7e3
yukitsubute <1318844659@qq.com>
2025-05-31 18:47:17 +0800

    fix(readme, config): 修改CDN链接

--- dist/tinygrail-magic.meta.js
+++ dist/tinygrail-magic.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Tinygrail Magic
 // @namespace    https://github.com/koorinyaa/tinygrail-magic
-// @version      1.0.0
+// @version      1.0.1
 // @author       koorinyaa
 // @description  全新的小圣杯界面
 // @icon         https://tinygrail.com/favicon.ico
--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 // 基础配置
 export const APP_NAME = 'Tinygrail Magic';
-export const APP_VERSION = '1.0.0';
+export const APP_VERSION = '1.0.1';
 export const APP_AUTHOR = 'koorinyaa';
 export const APP_DESCRIPTION = '全新的小圣杯界面';
 
 // 更新检查配置
-export const UPDATE_CHECK_URL = 'https://cdn.jsdelivr.net/gh/koorinyaa/tinygrail-magic@main/dist/tinygrail-magic.meta.js';
-export const UPDATE_URL = 'https://cdn.jsdelivr.net/gh/koorinyaa/tinygrail-magic@main/dist/tinygrail-magic.user.js';
+export const UPDATE_CHECK_URL = 'https://cdn.jsdelivr.net/gh/koorinyaa/tinygrail-magic@latest/dist/tinygrail-magic.meta.js';
+export const UPDATE_URL = 'https://cdn.jsdelivr.net/gh/koorinyaa/tinygrail-magic@latest/dist/tinygrail-magic.user.js';
