56d196a84586cb66286a51c962bbf6cafb675bda
yukitsubute <1318844659@qq.com>
2025-06-01 16:33:18 +0800

    chore(release): 更新dist

--- dist/tinygrail-magic.meta.js
+++ dist/tinygrail-magic.meta.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name         Tinygrail Magic
 // @namespace    https://github.com/koorinyaa/tinygrail-magic
-// @version      1.0.1
+// @version      1.0.2
 // @author       koorinyaa
 // @description  全新的小圣杯界面
 // @icon         https://tinygrail.com/favicon.ico
+// @downloadURL  https://cdn.jsdelivr.net/gh/koorinyaa/tinygrail-magic@main/dist/tinygrail-magic.user.js
+// @updateURL    https://cdn.jsdelivr.net/gh/koorinyaa/tinygrail-magic@main/dist/tinygrail-magic.meta.js
 // @match        *://*.bgm.tv/*
 // @match        *://*.bangumi.tv/*
 // @match        *://*.chii.in/*
