891bd240c75cdd949cda770147dc7bfdfbe5eb14
yukitsubute <1318844659@qq.com>
2025-06-07 10:47:24 +0800

    chore(release): 更新版本到1.0.3

--- dist/tinygrail-magic.meta.js
+++ dist/tinygrail-magic.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Tinygrail Magic
 // @namespace    https://github.com/koorinyaa/tinygrail-magic
-// @version      1.0.2
+// @version      1.0.3
 // @author       koorinyaa
 // @description  全新的小圣杯界面
 // @icon         https://tinygrail.com/favicon.ico
